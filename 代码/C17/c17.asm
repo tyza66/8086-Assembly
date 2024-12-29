e8693cadcc56a72e04c0c622392eb3206cb70aad
洮羱芝闇 <shun_@outlook.com>
2024-12-29 21:04:23 +0800

    更新代码

--- 代码/C17/c17.asm
+++ 代码/C17/c17.asm
@@ -4,7 +4,7 @@ comment *
 assume cs:codesg,ds:data,ss:stack
 
 data segment
-	str db 'Hello world'
+	str db 'Hello world','$'
 data ends
 
 stack segment
@@ -20,10 +20,17 @@ start:	mov ax,data	;先将ds指向数据区
 		
 s:		mov al,[bx]
 		and al,1011111B ;使用一种技巧进行变成大写的转换（相当于减去20） 这种技巧不会影响到原本就是大写字母的地方
-		;如果将and换成or就可以实现小写转换（相当于加上20）
+		;如果将and换成or al,100000B就可以实现小写转换（相当于加上20）
 		mov [bx],al
 		inc bx
 		loop s
+		
+		lea dx,str ;相当于mov dx,offset str
+		mov ah,9
+		int 21H
+		
+		mov ah,4cH
+		int 21H
 		
 codesg ends
 end start
---
+++ 代码/C18/c18.asm
@@ -0,0 +1,32 @@
+comment *
+这样希望最大值存在ah里面
+* comment
+assume cs:codesg,ds:data,ss:stack
+
+data segment
+	str db 'Hello world','$'
+data ends
+
+stack segment
+	db 10 dup(0)
+stack ends
+
+codesg segment
+start:	mov ax,data	;先将ds指向数据区
+		mov ds,ax
+		
+		mov bx,0	;用于记录偏移量
+		mov cx,11	;用于记录循环次数
+		
+		mov ah,00	;给ah先置为0 之后ah用于存储最大值
+		
+s:		mov al,[bx]	;将当前字符存进al
+		cmp ah,al	;比较ah和al 结果会存在标识寄存器中
+		jnb s1		;jnb是 如果 ah >= al (无符号数比较)，跳转到sl
+		mov ah,al	;如果没有跳转 就是因为 ah<al 那就将ah更新为当前的al
+
+s1:		inc bx		;bx步进一下
+		loop s	;如果cx-1不等于0那就回到s继续
+		
+codesg ends
+end start
