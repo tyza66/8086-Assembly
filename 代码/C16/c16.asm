1939754de55ae323acc67c415661e3a007ca1e29
洮羱芝闇 <shun_@outlook.com>
2024-12-29 10:16:16 +0800

    hello world

---
+++ 代码/C16/c16.asm
@@ -0,0 +1,21 @@
+assume cs:codesg,ds:data,ss:stack
+
+data segment
+	string db 'hello world','$' ;美元符号相当于是一个截至符
+data ends
+
+stack segment
+	db 10 dup(0)
+stack ends
+
+codesg segment
+start:	mov ax,data
+		mov ds,ax
+		mov dx,offset string
+		mov ah,9	;这样是输出
+		int 21H
+		
+		mov ah,4ch	;这样是安全退出
+		int 21H
+codesg ends
+end start
