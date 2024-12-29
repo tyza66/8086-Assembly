ac16a31c2df29f949177db04f5258062bba739da
洮羱芝闇 <shun_@outlook.com>
2024-12-29 19:42:13 +0800

    更新

--- 代码/C17/c17.asm
+++ 代码/C17/c17.asm
@@ -1,10 +1,10 @@
-* comment
-
+comment *
+这种也是注释
 * comment
 assume cs:codesg,ds:data,ss:stack
 
 data segment
-	string db 'hello world'
+	str db 'Hello world'
 data ends
 
 stack segment
@@ -12,6 +12,18 @@ stack segment
 stack ends
 
 codesg segment
-start:	
+start:	mov ax,data	;先将ds指向数据区
+		mov ds,ax
+		
+		mov bx,0	;用于记录偏移量
+		mov cx,11	;用于记录循环次数
+		
+s:		mov al,[bx]
+		and al,1011111B ;使用一种技巧进行变成大写的转换（相当于减去20） 这种技巧不会影响到原本就是大写字母的地方
+		;如果将and换成or就可以实现小写转换（相当于加上20）
+		mov [bx],al
+		inc bx
+		loop s
+		
 codesg ends
 end start
